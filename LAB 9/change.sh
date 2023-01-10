fa53f5d9cc8db165fc1ccf32400135f922f271c4
TusharBT15 <84076430+TusharBT15@users.noreply.github.com>
2023-01-10 21:18:27 +0530

    Add files via upload

---
+++ LAB 9/change.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#To change the permissions on a particular file
+ls -l workfile.txt
+chmod 711 workfile.txt
+ls -l workfile.txt
+chmod 520 workfile.txt
+ls -l workfile.txt
+chmod 776 workfile.txt
+ls -l workfile.txt
+
+
+
